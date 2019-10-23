dc66ac0b004e1e2187564e4b76b4b8031303d488
aldochu <aldochu@gmail.com>
2019-10-23 22:01:40 +0800

    Title will change according to "create" and "update"

---
+++ SSAD_Pancake/Assets/Script/ChangeAvatarTitle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeAvatarTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text title = GetComponent<Text>();
+        if (StaticVariable.lastVisited != null)
+        {
+            title.text = "Update Avatar";
+        }
+    }
+
+    // Update is called once per frame
+}
