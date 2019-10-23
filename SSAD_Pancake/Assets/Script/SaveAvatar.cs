196b101617fcea5f3ed3fc51e16146470ea65ba8
cedvogt <cedric.vogt@hispeed.ch>
2019-10-23 16:53:11 +0800

    updated Scripts

--- SSAD_Pancake/Assets/Script/SaveAvatar.cs
+++ SSAD_Pancake/Assets/Script/SaveAvatar.cs
@@ -43,7 +43,15 @@ public class SaveAvatar : MonoBehaviour
         StaticVariable.UserProfile.avatar.head = headIndex.ToString();
         StaticVariable.UserProfile.avatar.body = bodyIndex.ToString();
 
+		Debug.Log("LastVisited: " + StaticVariable.lastVisited);
 
+        if(StaticVariable.lastVisited == null)
+		{
+			Application.LoadLevel("Universe");
+		} else
+		{
+			Application.LoadLevel(StaticVariable.lastVisited);
+		}
 
     }
 }
--- SSAD_Pancake/Assets/worldToUniverseSceneSwitch.cs
+++ SSAD_Pancake/Assets/worldToUniverseSceneSwitch.cs
@@ -7,101 +7,106 @@ public class worldToUniverseSceneSwitch : MonoBehaviour
 
     public void GotoUniverseScene()
     {
-        SceneManager.LoadScene("Universe");
+		StaticVariable.lastVisited = "Universe";
+		Debug.Log(StaticVariable.lastVisited);
+		SceneManager.LoadScene("Universe");
     }
 
     public void GotoWorld1Scene()
     {
-        StaticVariable.world = "world1";
+		Debug.Log("Executed?" + StaticVariable.lastVisited);
+		StaticVariable.lastVisited = "world1";
+		StaticVariable.world = "world1";
         SceneManager.LoadScene("World1");
     }
 
     public void GotoWorld2Scene()
     {
-        StaticVariable.world = "world2";
+		StaticVariable.lastVisited = "world2";
+		StaticVariable.world = "world2";
         SceneManager.LoadScene("World2");
     }
 
     public void GotoGame1Easy()
     {
-        StaticVariable.game = 1;
+		StaticVariable.game = 1;
         StaticVariable.difficulty = "easy";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame1Medium()
     {
-        StaticVariable.game = 1;
+		StaticVariable.game = 1;
         StaticVariable.difficulty = "medium";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame1Hard()
     {
-        StaticVariable.game = 1;
+		StaticVariable.game = 1;
         StaticVariable.difficulty = "hard";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame2Easy()
     {
-        StaticVariable.game = 2;
+		StaticVariable.game = 2;
         StaticVariable.difficulty = "easy";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame2Medium()
     {
-        StaticVariable.game = 2;
+		StaticVariable.game = 2;
         StaticVariable.difficulty = "medium";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame2Hard()
     {
-        StaticVariable.game = 2;
+		StaticVariable.game = 2;
         StaticVariable.difficulty = "hard";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame3Easy()
     {
-        StaticVariable.game = 3;
+		StaticVariable.game = 3;
         StaticVariable.difficulty = "easy";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame3Medium()
     {
-        StaticVariable.game = 3;
+		StaticVariable.game = 3;
         StaticVariable.difficulty = "medium";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame3Hard()
     {
-        StaticVariable.game = 3;
+		StaticVariable.game = 3;
         StaticVariable.difficulty = "hard";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame4Easy()
     {
-        StaticVariable.game = 4;
+		StaticVariable.game = 4;
         StaticVariable.difficulty = "easy";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame4Medium()
     {
-        StaticVariable.game = 4;
+		StaticVariable.game = 4;
         StaticVariable.difficulty = "medium";
         SceneManager.LoadScene("CharacterSelection");
     }
 
     public void GotoGame4Hard()
     {
-        StaticVariable.game = 4;
+		StaticVariable.game = 4;
         StaticVariable.difficulty = "hard";
         SceneManager.LoadScene("CharacterSelection");
     }
@@ -112,11 +117,13 @@ public class worldToUniverseSceneSwitch : MonoBehaviour
         SceneManager.LoadScene("NewLB");
     }
 
-    public void GoToAvatar()
-    {
-        SceneManager.LoadScene("Avatar");
-    }
-    public void GoToCreateLevel()
+	public void GoToAvatar()
+	{
+		Debug.Log("why am i here");
+		StaticVariable.lastVisited = SceneManager.GetActiveScene().name;
+		SceneManager.LoadScene("Avatar");
+	}
+	public void GoToCreateLevel()
     {
         SceneManager.LoadScene("PlayerGame1");
     }
