544a89c31c25a53ea5e1979aaf7e0bf7d62e436e
aldochu <aldochu@gmail.com>
2019-11-02 03:25:49 +0800

    Added back changeScene class into the project

---
+++ SSAD_Pancake/Assets/Script/Application/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string sceneName;
+
+    public void changeScene(string sceneName)
+    {
+        Debug.Log(sceneName);
+        StaticVariable.lastVisited = SceneManager.GetActiveScene().name;
+        Application.LoadLevel(sceneName);
+    }
+    void OnMouseDown()
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
